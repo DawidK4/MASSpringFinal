301fad106752e0be2b53ab32a9eea2f69c06d70c
DawidK4 <s27637@pjwstk.edu.pl>
2025-06-10 00:43:31 +0200

    implemented association between OgloszenieWynajmu and AgentNieruchomosci

--- MAS/src/main/java/edu/pja/mas/dkucharski/mas/model/AgentNieruchomosci.java
+++ MAS/src/main/java/edu/pja/mas/dkucharski/mas/model/AgentNieruchomosci.java
@@ -9,12 +9,17 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
-@ToString
+@ToString(exclude = {"ogloszeniaWynajmu"}) // Wykluczamy kolekcję, aby uniknąć StackOverflowError
 public class AgentNieruchomosci {
 
     @Id
@@ -29,4 +34,27 @@ public class AgentNieruchomosci {
     @OneToOne(optional = false, cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "osoba_id", nullable = false, unique = true)
     private Osoba osoba;
+
+    // --- Nowa asocjacja z OgloszenieWynajmu ---
+    @OneToMany(mappedBy = "agentNieruchomosci", cascade = CascadeType.ALL, orphanRemoval = true)
+    // 'mappedBy' wskazuje pole w klasie OgloszenieWynajmu, które jest właścicielem relacji
+    private Set<OgloszenieWynajmu> zarzadza = new HashSet<>();
+    // ------------------------------------------
+
+    // Metody pomocnicze do zarządzania dwukierunkową relacją (dobra praktyka)
+    public void addOgloszenieWynajmu(OgloszenieWynajmu ogloszenie) {
+        if (this.zarzadza == null) {
+            this.zarzadza = new HashSet<>();
+        }
+        if (!this.zarzadza.contains(ogloszenie)) {
+            this.zarzadza.add(ogloszenie);
+            ogloszenie.setAgentNieruchomosci(this); // Ustawienie referencji powrotnej
+        }
+    }
+
+    public void removeOgloszenieWynajmu(OgloszenieWynajmu ogloszenie) {
+        if (this.zarzadza != null && this.zarzadza.remove(ogloszenie)) {
+            ogloszenie.setAgentNieruchomosci(null); // Usunięcie referencji powrotnej
+        }
+    }
 }
--- MAS/src/main/java/edu/pja/mas/dkucharski/mas/model/OgloszenieWynajmu.java
+++ MAS/src/main/java/edu/pja/mas/dkucharski/mas/model/OgloszenieWynajmu.java
@@ -12,7 +12,7 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
-@ToString
+@ToString // No need to exclude here unless you add a collection
 public class OgloszenieWynajmu {
 
     @Id
@@ -34,6 +34,13 @@ public class OgloszenieWynajmu {
     @JoinColumn(name = "nieruchomosc_id")
     @NotNull
     private Nieruchomosc zawiera;
+
+    // --- Nowa asocjacja z AgentNieruchomosci ---
+    @ManyToOne(optional = false) // OgloszenieWynajmu MUSI mieć AgentaNieruchomosci
+    @JoinColumn(name = "agent_nieruchomosci_id", nullable = false) // Nazwa kolumny klucza obcego
+    @NotNull // Dodatkowa walidacja, aby upewnić się, że pole nie jest null
+    private AgentNieruchomosci jestZarzadzanePrzez; // Pole do przechowywania referencji do AgentaNieruchomosci
+    // ---------------------------------------------
 
     public enum Status {
         AKTUALNE,
